49ca8429610433c04b2b86e59b1620d437d17b1c
Renan Ostrowski <renan_ostrowski@hotmail.com>
2020-03-31 12:10:19 -0300

    Iniciando Projeto Post

---
+++ Post/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace Post.Entities
+{
+    class Comment
+    {
+        public  string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
